4921365f2d82e501fcde3bb728049437f58565bb
Tom McLaughlin <tom@codedown.io>
2020-11-23 14:59:34 -0700

    Tidy

--- julia-to-nix-depot.py
+++ julia-to-nix-depot.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i python3 -p "python3.withPackages (ps: [ps.toml ps.GitPython])" nix-prefetch-git
 
-import os
 from pathlib import Path
 import sys
 import tempfile
