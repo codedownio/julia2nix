77055a8e2ad6dcba553a4b288f7205205af6fa62
Tom McLaughlin <tom@codedown.io>
2020-11-27 14:53:34 -0700

    Improve comments

--- templates/default.nix
+++ templates/default.nix
@@ -7,16 +7,14 @@ let
   # The base Julia version
   baseJulia = julia_13;
 
-  # Add extra library dependencies for your depot here!
-  # You might need to determine these by trial and error for your packages, i.e.
-  # build the depot and see if it complains about missing dependencies, then add
-  # them and repeat.
+  # Extra libraries for Julia's LD_LIBRARY_PATH.
+  # Recent Julia packages that use Artifacts.toml to specify their dependencies
+  # shouldn't need this.
+  # But if a package implicitly depends on some library being present, you can
+  # add it here.
+  extraLibs = [];
 
-  extraLibs = [
-
-  ];
-
-  # Wrapped Julia with libraries and environment variables
+  # Wrapped Julia with libraries and environment variables.
   # Note: setting The PYTHON environment variable is recommended to prevent packages
   # from trying to obtain their own with Conda.
   julia = runCommand "julia-wrapped" { buildInputs = [makeWrapper]; } ''
