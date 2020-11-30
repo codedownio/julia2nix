1427038ee7bb995ab99dd4e1dc44e9ac208e652b
Tom McLaughlin <tom@codedown.io>
2020-11-29 18:13:05 -0700

    Bump baseJulia

--- templates/default.nix
+++ templates/default.nix
@@ -5,7 +5,7 @@ with pkgs;
 
 let
   # The base Julia version
-  baseJulia = julia_13;
+  baseJulia = julia_15;
 
   # Extra libraries for Julia's LD_LIBRARY_PATH.
   # Recent Julia packages that use Artifacts.toml to specify their dependencies
