5568eb16622f3681b235f033f929fcce3d3a063c
Tom McLaughlin <tom@codedown.io>
2021-03-04 23:18:22 -0800

    Clarify comments in default.nix

--- templates/default.nix
+++ templates/default.nix
@@ -10,7 +10,7 @@ let
   # Extra libraries for Julia's LD_LIBRARY_PATH.
   # Recent Julia packages that use Artifacts.toml to specify their dependencies
   # shouldn't need this.
-  # But if a package implicitly depends on some library being present, you can
+  # But if a package implicitly depends on some library being present at runtime, you can
   # add it here.
   extraLibs = [];
 
@@ -37,6 +37,7 @@ callPackage ./common.nix {
   # You can add additional flags here.
   makeWrapperArgs = "";
 
-  # Extra Pkgs to precompile environment
+  # Extra buildInputs for building the Julia depot. Useful if your packages have
+  # additional build-time dependencies not managed through the Artifacts.toml system.
   extraBuildInputs = [];
 }
