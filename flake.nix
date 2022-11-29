78490765feddd123a802854c78243e7c6bee83a4
Tom McLaughlin <tom@codedown.io>
2022-11-29 03:05:37 -0700

    Try enabling darwin in flake

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+    flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
       in
