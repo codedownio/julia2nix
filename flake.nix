5ae77d870f13d02b8ac0a99bf81b2487a2793363
Tom McLaughlin <tom@codedown.io>
2022-11-29 03:21:36 -0700

    Revert "Try enabling darwin in flake"
    This reverts commit 78490765feddd123a802854c78243e7c6bee83a4.

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let
         pkgs = import nixpkgs { inherit system; };
       in
